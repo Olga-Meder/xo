eeef72fbf9e3fe50214bbb18d033731b808b7ac1
Olga Meder <aglo6a@gmail.com>
2021-06-08 13:27:00 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/ox1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ox1.dir/main.cpp.o"
+  "ox1"
+  "ox1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ox1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
